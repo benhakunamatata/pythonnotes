a3eb9836b0843e19209795d7ae4f249a79ce692b
Ben Ma <ben.hakunamatata@gmail.com>
2017-01-25 18:08:19 -0800

    Create runshellcmd.py

---
+++ runshellcmd.py
@@ -0,0 +1,15 @@
+'''run shell cmd'
+def run_cmd(cmd):
+    print 'cmd:'
+    print cmd
+    sp = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = sp.communicate()
+    print output
+    if output is not None:
+        output = output.strip()
+    # print error
+    if sp.returncode != 0:
+        print error
+    #sys.exit(-1)
+    return output
+    
